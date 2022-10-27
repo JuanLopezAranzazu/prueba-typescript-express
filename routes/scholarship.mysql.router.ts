c58eea3599cda0ad9b341c090889ce0b0c8b0781
Juan Esteban Lopez Aranzazu <juanlopezaranzazu@gmail.com>
2022-10-27 16:51:55 -0500

    update try catch next

--- routes/scholarship.mysql.router.ts
+++ routes/scholarship.mysql.router.ts
@@ -8,48 +8,60 @@ import { Scholarship } from "../models/mysqlScholarship";
 scholarshipRouter.get(
   "/",
   async (_req: Request, res: Response, next: NextFunction) => {
-    scholarshipMysqlService.findAll(
-      (err: Error, scholarships: Scholarship[]) => {
-        if (err) {
-          next(err);
+    try {
+      scholarshipMysqlService.findAll(
+        (err: Error, scholarships: Scholarship[]) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ data: scholarships });
         }
-        res.json({ data: scholarships });
-      }
-    );
+      );
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
 scholarshipRouter.get(
   "/:id",
   async (req: Request, res: Response, next: NextFunction) => {
-    const { params } = req;
-    const scholarshipId: number = Number(params.id);
-    scholarshipMysqlService.findOne(
-      scholarshipId,
-      (err: Error, scholarship: Scholarship) => {
-        if (err) {
-          next(err);
+    try {
+      const { params } = req;
+      const scholarshipId: number = Number(params.id);
+      scholarshipMysqlService.findOne(
+        scholarshipId,
+        (err: Error, scholarship: Scholarship) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ data: scholarship });
         }
-        res.json({ data: scholarship });
-      }
-    );
+      );
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
 scholarshipRouter.post(
   "/",
   async (req: Request, res: Response, next: NextFunction) => {
-    const { body } = req;
-    const newScholarshipEntry = toNewScholarshipsEntry(body);
-    scholarshipMysqlService.create(
-      newScholarshipEntry,
-      (err: Error, scholarshipId: number) => {
-        if (err) {
-          next(err);
+    try {
+      const { body } = req;
+      const newScholarshipEntry = toNewScholarshipsEntry(body);
+      scholarshipMysqlService.create(
+        newScholarshipEntry,
+        (err: Error, scholarshipId: number) => {
+          if (err) {
+            throw err;
+          }
+          res.status(201).json({ scholarshipId });
         }
-        res.status(201).json({ scholarshipId });
-      }
-    );
+      );
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
--- routes/user.mysql.router.ts
+++ routes/user.mysql.router.ts
@@ -9,79 +9,95 @@ import { User } from "../models/mysqlUser";
 userRouter.get(
   "/",
   async (_req: Request, res: Response, next: NextFunction) => {
-    userMysqlService.findAll((err: Error, users: User[]) => {
-      if (err) {
-        next(err);
-      }
-      res.json({ data: users });
-    });
+    try {
+      userMysqlService.findAll((err: Error, users: User[]) => {
+        if (err) {
+          throw err;
+        }
+        res.json({ data: users });
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
 userRouter.get(
   "/:id",
   async (req: Request, res: Response, next: NextFunction) => {
-    const { params } = req;
-    const userId: number = Number(params.id);
-    userMysqlService.findOne(userId, (err: Error, user: User) => {
-      if (err) {
-        next(err);
-      }
-      res.json({ data: user });
-    });
+    try {
+      const { params } = req;
+      const userId: number = Number(params.id);
+      userMysqlService.findOne(userId, (err: Error, user: User) => {
+        if (err) {
+          throw err;
+        }
+        res.json({ data: user });
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
 userRouter.post(
   "/",
   async (req: Request, res: Response, next: NextFunction) => {
-    const { body } = req;
-    const { fullname, email, password, scholarship, role } = body;
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
-    const dataForUser = {
-      fullname,
-      email,
-      password: passwordHash,
-      scholarship,
-      role,
-    };
-    const newUserEntry = toNewUserEntry(dataForUser);
-    userMysqlService.create(newUserEntry, (err: Error, userId: number) => {
-      if (err) {
-        next(err);
-      }
-      res.status(201).json({ userId });
-    });
+    try {
+      const { body } = req;
+      const { fullname, email, password, scholarship, role } = body;
+      const saltRounds = 10;
+      const passwordHash = await bcrypt.hash(password, saltRounds);
+      const dataForUser = {
+        fullname,
+        email,
+        password: passwordHash,
+        scholarship,
+        role,
+      };
+      const newUserEntry = toNewUserEntry(dataForUser);
+      userMysqlService.create(newUserEntry, (err: Error, userId: number) => {
+        if (err) {
+          throw err;
+        }
+        res.status(201).json({ userId });
+      });
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
 userRouter.put(
   "/:id",
   async (req: Request, res: Response, next: NextFunction) => {
-    const { body, params } = req;
-    const userId = Number(params.id);
-    const { fullname, email, password, scholarship, role } = body;
-    const saltRounds = 10;
-    const passwordHash = await bcrypt.hash(password, saltRounds);
-    const dataForUser = {
-      fullname,
-      email,
-      password: passwordHash,
-      scholarship,
-      role,
-    };
-    const newUserEntry = toNewUserEntry(dataForUser);
-    userMysqlService.update(
-      userId,
-      newUserEntry,
-      (err: Error, info: string) => {
-        if (err) {
-          next(err);
+    try {
+      const { body, params } = req;
+      const userId = Number(params.id);
+      const { fullname, email, password, scholarship, role } = body;
+      const saltRounds = 10;
+      const passwordHash = await bcrypt.hash(password, saltRounds);
+      const dataForUser = {
+        fullname,
+        email,
+        password: passwordHash,
+        scholarship,
+        role,
+      };
+      const newUserEntry = toNewUserEntry(dataForUser);
+      userMysqlService.update(
+        userId,
+        newUserEntry,
+        (err: Error, info: string) => {
+          if (err) {
+            throw err;
+          }
+          res.json({ info });
         }
-        res.json({ info });
-      }
-    );
+      );
+    } catch (err) {
+      next(err);
+    }
   }
 );
 
